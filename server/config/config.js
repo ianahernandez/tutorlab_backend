1d71c1dc169116e11183bcbf1c9fae49f12b371e
Ana <anaceciliah95@gmail.com>
2019-10-09 23:03:45 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://172.17.0.2:27017/tutorlab';
 } else {
-    // urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb+srv://tutorlab:ZE4u7hEHigVfSSjW@cluster0-kheys.mongodb.net/tutorlab'
+    urlDB = process.env.MONGO_URI;
+    // urlDB = 'mongodb+srv://tutorlab:ZE4u7hEHigVfSSjW@cluster0-kheys.mongodb.net/tutorlab'
 }
 process.env.URLDB = urlDB;
